3f5af6cb940f2e853880392ad88f8ab1835dd59b
skmtkytr <skmtkytr@gmail.com>
2017-12-11 12:54:06 +0900

    update

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/config/nvim/init.vim ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.comf
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/config ~/.config
+ln -sf ~/dotfiles/.ctags ~/.ctags
+
