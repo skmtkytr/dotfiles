216830577a821937ea70a3db0b503e2e706b3255
skmtkytr <skmtkytr@gmail.com>
2023-07-18 15:22:54 +0900

    update first time auto configure

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,8 +2,8 @@
 
 ln -sf ~/dotfiles/config/nvim/init.vim ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/config ~/.config
 ln -sf ~/dotfiles/.ctags ~/.ctags
-ln -sf ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -sf ~/dotfiles/config/.wezterm.lua ~/.wezterm.lua
+
