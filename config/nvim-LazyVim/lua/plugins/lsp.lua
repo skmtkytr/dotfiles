864dd8e6df0df48d040b6a5417232f11e906203f
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2024-05-13 11:54:15 +0900

    update

--- config/nvim-LazyVim/lua/plugins/lsp.lua
+++ config/nvim-LazyVim/lua/plugins/lsp.lua
@@ -1,3 +1,27 @@
 -- lsp plugins
 
-return {}
+return {
+  {
+    "glepnir/lspsaga.nvim",
+    event = { "LspAttach" },
+    keys = {
+      { "K", "<cmd>Lspsaga hover_doc<CR>" },
+      { "gr", "<cmd>Lspsaga finder<CR>" },
+      { "gd", "<cmd>Lspsaga peek_definition<CR>" },
+      { "gdv", ":vsplit | lua vim.lsp.buf.definition()<CR>" },
+      { "ga", "<cmd>Lspsaga code_action<CR>" },
+      { "gn", "<cmd>Lspsaga rename<CR>" },
+      { "<S-o>", "<cmd>Lspsaga outline<CR>" },
+      { "ge", "<cmd>Lspsaga show_line_diagnostics<CR>" },
+      { "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>" },
+      { "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>" },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lspsaga").setup()
+    end,
+  }, -- LSP UIs
+}
