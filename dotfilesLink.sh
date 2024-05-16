b22f9f4ed5be5fd1e3a0f067af6b24a1bdd19f97
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2024-05-16 15:38:18 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/config/nvim/init.vim ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/config ~/.config
-ln -sf ~/dotfiles/.ctags ~/.ctags
-ln -sf ~/dotfiles/config/.wezterm.lua ~/.wezterm.lua
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-ln -sf ~/dotfiles/.hammerspoon ~/.hammerspoon
+ln -sf $SCRIPT_DIR/config/nvim/init.vim ~/.vimrc
+ln -sf $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+ln -sf $SCRIPT_DIR/.gitconfig ~/.gitconfig
+ln -sf $SCRIPT_DIR/config ~/.config
+ln -sf $SCRIPT_DIR/.ctags ~/.ctags
+ln -sf $SCRIPT_DIR/config/.wezterm.lua ~/.wezterm.lua
+ln -sf $SCRIPT_DIR/.hammerspoon ~/.hammerspoon
