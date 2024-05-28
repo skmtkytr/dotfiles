88521e15ac342a0b1740ff898f7002f47a801dd5
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2024-05-28 17:58:50 +0900

    update

--- config/nvim-LazyVim/lua/plugins/git.lua
+++ config/nvim-LazyVim/lua/plugins/git.lua
@@ -7,11 +7,10 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = function(_, opts)
-      opts.current_line_blame = opts.current_line_blame or {}
-      table.insert(opts.current_line_blame, { true })
+      opts["current_line_blame"] = true
     end,
-    config = function()
-      require("gitsigns").setup()
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
       require("scrollbar.handlers.gitsigns").setup()
     end,
   },
