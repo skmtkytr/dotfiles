aea92fa2aa1b9c438bfd941bda633d9afca8d119
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2024-05-28 14:47:04 +0900

    update

--- config/nvim-LazyVim/lua/plugins/ruby.lua
+++ config/nvim-LazyVim/lua/plugins/ruby.lua
@@ -11,10 +11,8 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "ruby",
       })
-      opts.endwise = opts.endwise or {}
-      vim.list_extend(opts.endwise, {
-        enable = true,
-      })
+
+      opts["endwise"] = { enable = true }
     end,
   },
   -- {
