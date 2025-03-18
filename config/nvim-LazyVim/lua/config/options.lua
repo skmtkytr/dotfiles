eb223969603b32cd474805477238f164be1c290e
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2025-03-19 08:50:16 +0900

    update

--- config/nvim-LazyVim/lua/config/options.lua
+++ config/nvim-LazyVim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 if vim.g.neovide then
-  vim.o.guifont = "UDEV Gothic 35NFLG,Symbols Nerd Font Mono:h40"
-  vim.g.neovide_scale_factor = 0.3
+  vim.o.guifont = "UDEV Gothic 35NFLG,Symbols Nerd Font Mono:h13"
+  vim.g.neovide_scale_factor = 1
   vim.opt.linespace = 1
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
   vim.g.neovide_cursor_antialiasing = true
