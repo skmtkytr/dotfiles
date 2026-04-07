4217996c72aa958ee8a8c52cea0c2ba4fa814a30
Kyotaro Sakamoto / skmtkytr <skmtkytr+github@gmail.com>
2026-04-07 14:39:34 +0900

    feat: add Linux-only configs (Hyprland/KDE/Wayland desktop)

---
+++ dot_config/start_Hyprland.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh
+export XDG_CURRENT_DESKTOP=Hyprland
+export XDG_SESSION_DESKTOP=Hyprland
+export XDF_SESSION_TYPE=Hyprland
+export WLR_NO_HARDWARE_CURSORS=1
+export WLR_RENDERER_ALLOW_SOFTWARE=1
+exec Hyprland
